1f9bb8fe11c840d3b59f26f04bf3dffaf548104e
Jeremy Bush <contractfrombelow@gmail.com>
2012-09-04 20:38:18 -0500

    really basic router working

---
+++ classes/kohana/router.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Kohana;
+
+class Router
+{
+
+  const REGEX_ESCAPE  = '[.\\+*?[^\\]${}=!|]';
+  const REGEX_SEGMENT = '[a-zA-Z0-9_]++';
+//  const REGEX_SEGMENT = '[^/.,;?\n]++';
+
+  protected $_routes = [
+    'get' => [],
+  ];
+
+  public function get($uri, $action)
+  {
+    $this->_routes['get'][$uri] = $action;
+  }
+
+  public function match($method, $uri, $regex = [])
+  {
+    $uri = ltrim($uri, '/');
+
+    foreach ($this->_routes[$method] as $stored_uri => $action)
+    {
+      $expression = preg_replace('#'.Router::REGEX_ESCAPE.'#', '\\\\$0', $stored_uri);
+      $expression = str_replace(array('<', '>'), array('(?P<', '>'.Router::REGEX_SEGMENT.')'), $expression);
+
+      $expression = '#^'.$expression.'$#uD';
+
+      if ($stored_uri == $uri)
+        return $action();
+
+      if (preg_match($expression, $uri, $matches))
+      {
+        foreach ($matches as $k => $v)
+        {
+          if ( ! is_int($k))
+          {
+            $this->_params[$k] = $v;
+          }
+        }
+        return $action($matches);
+      }
+    }
+  }
+
+  public function params($param = NULL)
+  {
+    return $this->_params[$param];
+  }
+}
---
+++ specs/RouterSpec.php
@@ -0,0 +1,28 @@
+<?php
+
+include 'classes/kohana/router.php';
+
+class DescribeRouter extends \PHPSpec\Context
+{
+  public function itMatchesAUri()
+  {
+    $router = new \Kohana\Router;
+    $router->get('', function() {
+      return 'the content';
+    });
+
+    $this->spec($router->match('get', '/'))->should->be('the content');
+  }
+
+  public function itMatchesAUriWithParameters()
+  {
+    $router = new \Kohana\Router;
+    $router->get('<asked>/<got>', function() use($router) {
+      $asked = $router->params('asked');
+      $got = $router->params('got');
+      return "You asked for $asked but I gave you $got!";
+    });
+
+    $this->spec($router->match('get', '/1/2'))->should->be('You asked for 1 but I gave you 2!');
+  }
+}
